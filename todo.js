b2a1b02f5f159a5a57ac3a42b4c0fd9b2de83a2e
Stavros Kollias <kollias1252@gmail.com>
2020-08-06 12:51:33 +0100

    added validation for empty string inputs

--- todo.js
+++ todo.js
@@ -7,22 +7,35 @@ function handleSaveNewItem() {
   const newNameInput = document.getElementById("new-name");
   const newDateInput = document.getElementById("new-due-date");
   const newDescriptionInput = document.getElementById("new-description");
-  const toDoList = document.querySelector(".to-do-list");
-  const toDoItem = document.querySelector(".template");
-  const newToDo = cloneElement(toDoItem);
-  changeClassFromElement(newToDo, "to-do-item");
-  const newToDoDetails = newToDo.children[0];
-  changeTextToElement(newToDoDetails.children[0], newTitleInput.value);
-  changeTextToElement(newToDoDetails.children[1], newDateInput.value);
-  changeTextToElement(newToDoDetails.children[2], newNameInput.value);
-  changeTextToElement(newToDoDetails.children[3], newDescriptionInput.value);
 
-  addChildToELement(toDoList, newToDo);
+  const inputs = document.querySelectorAll(".new-input-to-do");
+  const result = forEachThroughEelements(inputs, validateInputsNotEmpty);
 
-  clearAllInputsNewToDo();
+  if (!result) {
+    const toDoList = document.querySelector(".to-do-list");
+    const toDoItem = document.querySelector(".template");
+    const newToDo = cloneElement(toDoItem);
+    changeClassFromElement(newToDo, "to-do-item");
+    const newToDoDetails = newToDo.children[0];
+    changeTextToElement(newToDoDetails.children[0], newTitleInput.value);
+    changeTextToElement(newToDoDetails.children[1], newDateInput.value);
+    changeTextToElement(newToDoDetails.children[2], newNameInput.value);
+    changeTextToElement(newToDoDetails.children[3], newDescriptionInput.value);
+
+    addChildToELement(toDoList, newToDo);
+
+    clearAllInputsNewToDo();
+  }
 }
 
-function validateInputsNotEmpty() {}
+function forEachThroughEelements(list, operationFunction) {
+  var result = "";
+  list.forEach((e, i) => {
+    const operationResult = operationFunction(e);
+    operationResult ? (result += 1) : result;
+  });
+  return result;
+}
 
 function deleteElementFromItsParent(element) {
   const parent = element.parentElement.parentElement.parentElement;
@@ -31,7 +44,19 @@ function deleteElementFromItsParent(element) {
   removeChildFromElement(parent, child);
 }
 
-// ----functions-----------------
+// --------functions------------functions------functions-------------------
+
+function isEmptyString(str) {
+  return str == "";
+}
+
+function validateInputsNotEmpty(element) {
+  const isEmpty = isEmptyString(element.value);
+  isEmpty
+    ? addClassToElement(element, "error-input")
+    : removeClassFromElement(element, "error-input");
+  return isEmpty;
+}
 
 function changeClassFromElement(element, className) {
   element.className = className;
@@ -74,4 +99,10 @@ function removeChildFromElement(element, child) {
   element.removeChild(child);
 }
 
-function isEmptyString(sting) {}
+function addClassToElement(element, className) {
+  element.classList.add(className);
+}
+
+function removeClassFromElement(element, className) {
+  element.classList.remove(className);
+}
